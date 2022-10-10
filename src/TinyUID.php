ab7bbcb13e7565f7573ca4b54653571fef0e9d31
Shinichiro Yabu <scin.ichi@gmail.com>
2022-10-10 23:12:16 +0900

    🐔

--- src/TinyUID.php
+++ src/TinyUID.php
@@ -3,18 +3,27 @@
 namespace piyo2\util\tinyuid;
 
 use ErrorException;
+use piyo2\util\base\Base;
+
+require __DIR__ . '/../vendor/piyo2/base/src/Base.php';
 
 final class TinyUID
 {
-	public static function generate(int $length = 14): string
+	/** @var string */
+	const BASE_DEFAULT = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+	public static function generate(int $length = 14, string $base = self::BASE_DEFAULT): string
 	{
+		$byteLength = ceil($length * log(strlen($base)) / log(2) / 8);
+
 		if (\is_callable('random_bytes')) {
-			$bytes = random_bytes(ceil($length / 2));
+			$bytes = random_bytes($byteLength);
 		} elseif (\is_callable('openssl_random_pseudo_bytes')) {
-			$bytes = openssl_random_pseudo_bytes(ceil($length / 2));
+			$bytes = \openssl_random_pseudo_bytes($byteLength);
 		} else {
 			throw new ErrorException('No cryptographically secure random function available');
 		}
-		return substr(bin2hex($bytes), 0, $length);
+
+		return \substr(Base::convert(\bin2hex($bytes), '0123456789abcdef', $base), -$length);
 	}
 }
---
+++ tests/stat.php
@@ -0,0 +1,26 @@
+<?php
+
+use piyo2\util\tinyuid\TinyUID;
+
+require __DIR__ . '/../src/TinyUID.php';
+
+$length = 14;
+$N = 100000;
+
+$base = 62;
+$lineWidth = 76;
+
+$bucket = [];
+foreach (range(1, $N) as $i) {
+	$uid = TinyUID::generate($length);
+	for ($i = 0; $i < strlen($uid); $i++) {
+		if (!isset($bucket[$uid[$i]])) $bucket[$uid[$i]] = 0;
+		$bucket[$uid[$i]]++;
+	}
+}
+
+ksort($bucket, SORT_STRING);
+
+foreach ($bucket as $char => $frequency) {
+	echo $char, ' ', str_repeat('*', round($lineWidth * $frequency / $N / $length * $base)), PHP_EOL;
+}
